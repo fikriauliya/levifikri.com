cb34782ad71a16ecc37d685ddd91c2688caab699
Pahlevi Fikri Auliya <levi@ruangguru.com>
2024-03-31 16:00:44 +0700

    docs: finished the structed

---
+++ rust/borrow/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+type Person struct {
+	name string
+	age  uint8
+}
+
+func first(v *[]Person) *Person {
+	return &(*v)[0]
+}
+
+func testSlices() {
+	people := []Person{
+		{
+			name: "Alice",
+			age:  30,
+		},
+	}
+
+	firstPerson := first(&people)
+	fmt.Println(firstPerson.name)
+
+	// remove the first element
+	_ = slices.Delete(people, 0, 1)
+
+	fmt.Println(firstPerson.name)
+}
+
+func main() {
+	testSlices()
+}
